220c5466fb9062800e79581cc8ee5795e5818f9c
Tim Zinsky <tzinsky@gmail.com>
2017-01-05 09:33:54 -0600

    Updated SimpleREST client

--- simpleREST/simpleREST.go
+++ simpleREST/simpleREST.go
@@ -1,21 +1,37 @@
 package main
 
 import (
-    "net/http"
-    "log"
-    "github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
-func response (rw http.ResponseWriter, request *http.Request) {
-    log.Printf("ACTION: %s - Remote: %s Local URI: %s",request.Method, 
-                                             request.RemoteAddr,
-                                             request.RequestURI)
-    rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    rw.Write([]byte ("Hello world!"))
+func Logger(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		inner.ServeHTTP(w, r)
+
+		log.Print("%s\t%s\t%s\t%s",
+			r.Method,
+			r.RequestURI,
+			name,
+			time.Since(start))
+	})
+}
+
+func response(rw http.ResponseWriter, request *http.Request) {
+	log.Printf("ACTION: %s - Remote: %s Local URI: %s", request.Method,
+		request.RemoteAddr,
+		request.RequestURI)
+	rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	rw.Write([]byte("Hello world!"))
 }
 
 func main() {
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", response)
-    log.Fatal(http.ListenAndServe (":8080", router))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", response)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
